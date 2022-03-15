f465b9c231e07a41b5a780f57ee70aca85614f49
Steve Whitney <steve.whitney@gmail.com>
2022-03-15 16:37:06 +0000

    challenge-6-part-2

--- challenges/challenge_6.rb
+++ challenges/challenge_6.rb
@@ -10,4 +10,6 @@ if __FILE__ == $PROGRAM_NAME
   population = LanternFishPopulation.new(lines.first.split(',').map(&:to_i))
   population.pass_days(80)
   puts population.number_of_fish
+  population.pass_days(176)
+  puts population.number_of_fish
 end
